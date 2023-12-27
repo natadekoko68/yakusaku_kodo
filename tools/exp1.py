1563f438d9a2e665f1f594d698b9d1d17619a84e
natadekoko68 <125146909+natadekoko68@users.noreply.github.com>
2023-12-27 20:48:38 +0900

    Changes

---
+++ tools/exp1.py
@@ -0,0 +1,85 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import re
+import japanize_matplotlib
+import warnings
+warnings.simplefilter("ignore")
+
+input = "/Users/kotaro/PycharmProjects/yakusaku_kodo/input/"
+df = pd.read_csv(input + "data_yakusaku_gakusyu.csv").drop('group_num', axis=1)
+# output = "/Users/kotaro/Desktop/yakusaku_output/"
+output = "./output/"
+
+def make_label_value(df):
+    labels = {}
+    values = {}
+    for key in ["att", "fix", "rev"]:
+        labels[key] = []
+        values[key] = []
+        for col in df.columns:
+            for i in range(len(df[col])):
+                if re.search("day[0-9]_" + key + "_.+", col):
+                    label = ""
+                    if "day1" in col:
+                        label += "1日目"
+                    else:
+                        label += "2日目"
+                    label += "\n"
+                    if "sa" in col:
+                        label += "生理食塩水"
+                    else:
+                        label += "スコポラミン"
+                    labels[key].append(label)
+                    values[key].append(df.loc[i, col])
+    return labels, values
+
+def make_dict(df):
+    ret = {}
+    a, b = make_label_value(df)
+    for key in ["att", "fix", "rev"]:
+        dict_temp = dict(labels=a[key], values=b[key])
+        ret[key] = pd.DataFrame(dict_temp)
+    return ret
+
+
+def graph_all(df):
+    dict = make_dict(df)
+    cnt = 0
+    lab = ["獲得","固定","再生"]
+    fig = plt.figure(figsize=(12,5))
+    order = ["1日目\n生理食塩水", "2日目\n生理食塩水", "1日目\nスコポラミン", "2日目\nスコポラミン"]
+    for key in dict:
+        ax = plt.subplot(1, 3, cnt+1)
+        sns.swarmplot(dict[key], x="labels", y="values", order=order, palette="Set2", size=5, legend=False)
+        sns.boxplot(dict[key], x="labels", y="values", color="white", order=order, legend=False)
+        plt.title("実験1(" + lab[cnt] + ")")
+        ax.set(xlabel='', ylabel='時間(sec)')
+        plt.xticks(size=5)
+        cnt += 1
+    plt.tight_layout()
+    plt.savefig(output + "実験1_all.jpg", dpi=300)
+
+def graphs(df):
+    dict = make_dict(df)
+    cnt = 0
+    lab = ["獲得", "固定", "再生"]
+    order = ["1日目\n生理食塩水", "2日目\n生理食塩水", "1日目\nスコポラミン", "2日目\nスコポラミン"]
+    for key in dict:
+        fig = plt.figure()
+        ax = plt.subplot(1, 1, 1)
+        sns.swarmplot(dict[key], x="labels", y="values", order=order, palette="Set2", size=5)
+        sns.boxplot(dict[key], x="labels", y="values", color="white", order=order)
+        plt.title("実験1(" + lab[cnt] + ")")
+        # plt.xticks(size=5)
+        ax.set(xlabel='', ylabel='時間(sec)')
+        plt.tight_layout()
+        plt.savefig(output + "実験1("+lab[cnt]+").jpg", dpi=300)
+        plt.close()
+        cnt += 1
+
+
+if __name__ == "__main__":
+    graph_all(df)
+    graphs(df)
+
---
+++ tools/exp2.py
@@ -0,0 +1,40 @@
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import japanize_matplotlib
+import warnings
+warnings.simplefilter("ignore")
+
+input = '/Users/kotaro/PycharmProjects/yakusaku_kodo/input/data_yakusaku_saimin.csv'
+df_exp2 = pd.read_csv(input).drop("group_num", axis=1)
+output = "/Users/kotaro/Desktop/yakusaku_output/"
+
+
+def graph_exp2(df):
+    labels = []
+    values = []
+
+    for col in df.columns:
+        for i in range(len(df)):
+            labels.append(col)
+            values.append(df.loc[i,col])
+
+    dic = dict(labels=labels, values=values)
+    df = pd.DataFrame.from_dict(dic)
+
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    sns.violinplot(df, x="labels", y="values", color="white")
+    sns.swarmplot(df, x="labels", y="values", palette="Set2")
+    ax.set(xlabel="投与薬物", ylabel="時間(sec)")
+    plt.title("投与薬物と正向反射消失時間の関係")
+
+    plt.tight_layout()
+    plt.savefig(output + '実験2.jpg',dpi=300)
+    plt.show()
+
+if __name__ == '__main__':
+    graph_exp2(df_exp2)
+
+
+
